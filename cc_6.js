00640a33a4b524c655b2d2a0d0969053913bf703
Joshverde <52259302+Joshverde@users.noreply.github.com>
2025-02-09 13:21:51 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -6,3 +6,13 @@ function calculateProfit(costPrice,selling,unitsSold){
 };
 
 console.log(`**Task 1**\nTotal Profit:${calculateProfit(20,30,100)}\nTotal Profit: ${calculateProfit(50,70,200)}\n`);
+
+
+//Task 2 - Sales Tax Computation
+function calculateSalesTax(amount,taxRate){
+    // calculates the sales tax of the given amount 
+    salesTax = amount *taxRate
+    return salesTax
+};
+
+console.log(`**Task 2**\nSales Tax: ${calculateSalesTax(100,.07)}\nSales Tax: ${calculateSalesTax(500,.1)}\n`);
